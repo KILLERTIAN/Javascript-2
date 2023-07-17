51464b7953531b07a0ef5da372c95ca8fd44fb33
Om Sharma <omsharma050322@gmail.com>
2023-07-17 23:17:35 +0530

    Initial Commit

---
+++ Strict mode/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Strict mode help us identify errors which don't get logged out generally in javascript
+
+let hasdriversLicence = 'false';
+const passTest = true;
+
+if (passTest) hasdriverLicence = true;
+if (hasdriversLicence) console.log("I can drive :D");
