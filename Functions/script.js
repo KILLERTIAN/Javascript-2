cbe52df969714f96a40fc7e8e2bd59c0236af99c
Om Sharma <omsharma050322@gmail.com>
2023-07-19 23:54:06 +0530

    Added function example

--- Functions/script.js
+++ Functions/script.js
@@ -3,5 +3,18 @@ function logger() {
     console.log('My name is Jonas');
 }
 
-// calling /running or invoking a funtion
+// calling /running or invoking funtion
 logger();
+
+function fruitprocessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
+}
+
+fruitprocessor(5, 6);
+// output 5 6
+
+const applejuice = fruitprocessor(5, 6);
+console.log(applejuice);
+// output Juice with 5 apples and 6 oranges
