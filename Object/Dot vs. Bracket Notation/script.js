22712674a8f1ad0ad6218571aca15c879b2ba089
Om Sharma <omsharma050322@gmail.com>
2023-07-31 23:52:30 +0530

    Dot vs Bracket

---
+++ Object/Dot vs. Bracket Notation/script.js
@@ -0,0 +1,21 @@
+//Creating Objects
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Micheal', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schedtmann',
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['Micheal', 'Peter', 'Steven']
+}
+console.log(jonas);
+
+// To get lastname from the object
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
