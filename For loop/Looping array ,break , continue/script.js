13636c645b6075b55ce5b2031ca3f0682a85bc7a
Om Sharma <omsharma050322@gmail.com>
2023-08-14 23:36:07 +0530

    comments

--- For loop/Looping array ,break , continue/script.js
+++ For loop/Looping array ,break , continue/script.js
@@ -1,5 +1,7 @@
+// Array on which our loop is working
 const jonas = [Firstname, 'schmedtmann', 2037 - 1991, 'teacher', friends];
 
+// for loop which prints all the values in the array 
 for (let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof (jonas[i]));
 }
