633dfe53721f74d5926233833cc8791855d7ca4f
Om Sharma <omsharma050322@gmail.com>
2023-08-14 23:39:49 +0530

    filling a array

--- For loop/Looping array ,break , continue/script.js
+++ For loop/Looping array ,break , continue/script.js
@@ -1,7 +1,13 @@
 // Array on which our loop is working
 const jonas = [Firstname, 'schmedtmann', 2037 - 1991, 'teacher', friends];
 
+//Creating a new array 
+const types = [];
+
 // for loop which prints all the values in the array 
 for (let i = 0; i < jonas.length; i++) {
-    console.log(jonas[i], typeof (jonas[i]));
+    console.log(jonas[i], typeof jonas[i]);
+
+    //Filling the new array
+    types[i] = typeof jonas[i];
 }
