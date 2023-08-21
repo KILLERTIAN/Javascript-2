98fd3a17223d2207e29eccf53b22468bcaed7044
Om Sharma <omsharma050322@gmail.com>
2023-08-21 23:13:52 +0530

    loop inside a loop

--- For loop/Looping array ,break , continue/script.js
+++ For loop/Looping array ,break , continue/script.js
@@ -52,3 +52,11 @@ const jonas1 = [
 for (let i = jonas.length - 1; i >= 0; i--) {
     console.log(i, jonas[i]);
 }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-------- Starting Exercise ${exercise}`);
+    // Loop inside a loop
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
+}
